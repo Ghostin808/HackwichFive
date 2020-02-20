cb54d7973578c6d0c523244e3b5e4a966c98e49d
Joseph Payongayong <spbeachhi@gmail.com>
2020-02-20 09:59:30 -1000

    Completed Parts 1-7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Variable declarations
+    var currentIndex = 0
+    
+    var foodNames = ["Burgers" , "Sushi" , "Mexican" , "Poke" , "Lau lau"]
 
-    override func viewDidLoad() {
+    @IBOutlet weak var topLabelPressed: UILabel!
+    
+    @IBOutlet weak var bottomLabelPressed: UILabel!
+    
+    override func viewDidLoad(){
+        self.topLabelPressed.text = "My Favorite Foods"
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        self.bottomLabelPressed.text = foodNames[currentIndex]
+        
+    }
+    
 }
 
