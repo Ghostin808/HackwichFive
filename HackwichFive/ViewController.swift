a29f971cd4a23cf5c277f65688746d8ad9f081bd
Joseph Payongayong <spbeachhi@gmail.com>
2020-02-20 10:25:16 -1000

    Completed Parts 1-8

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -18,16 +18,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bottomLabelPressed: UILabel!
     
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     override func viewDidLoad(){
         self.topLabelPressed.text = "My Favorite Foods"
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func buttonPressed(_ sender: Any) {
+    @IBAction func buttonPressed(_ sender: Any) { //the button was pressed, will initiate tasks below
         
-        self.bottomLabelPressed.text = foodNames[currentIndex]
-        
+        self.bottomLabelPressed.text = foodNames[currentIndex] //shows the name of my favorite food at given index
+        if self.currentIndex < self.foodNames.count //making sure that the index is less than the count of the number of foods
+        {
+            currentIndex+=1 //used to move onto the the next food
+            
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)//when button is pressed once, the title displays"Next"
+        }
+        else
+        {
+            (sender as! UIButton).isEnabled = false //no more strings in array
+        }
     }
     
 }
